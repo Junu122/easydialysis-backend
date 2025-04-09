44a35ced6f4b58b2fa30c0dafee7c149adfd96ab
junaid <junaid.sheikh.1800@gmail.com>
2025-04-10 00:29:27 +0530

    added vercel url

--- index.js
+++ index.js
@@ -10,8 +10,9 @@ const app=express()
  
 
 app.use(cors({
-    origin: 'http://localhost:5173',
-    credentials:true
+    origin: 'https://easydialysis-frontend.vercel.app',
+    credentials:true,
+    methods: ["GET", "POST", "PUT", "DELETE"]
 }))
 app.use(cookieParser());
 
