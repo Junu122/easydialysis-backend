fcc2081a18b0e8a93e65da4c9a9595b8d57c26b2
junaid <junaid.sheikh.1800@gmail.com>
2025-04-12 12:05:48 +0530

    change the cors setting

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app=express()
 
 
 app.use(cors({
-    origin: 'https://easydialysis.shop',
+    origin: 'https://www.easydialysis.shop',
     credentials:true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
